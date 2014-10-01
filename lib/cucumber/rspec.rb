d86149b81b96379b41f40b261c93a2d179593977
Steve Tooke <steve@boxjump.co.uk>
2014-10-01 12:07:37 +0100

    Support older versions of RSpec
    I'm not sure this is really necessary, cucumber 2.0 probably doesn't
    need to support pre 3.0 versions of RSpec?

--- lib/cucumber/rspec.rb
+++ lib/cucumber/rspec.rb
@@ -1,3 +1,14 @@
 require "cucumber/rspec/version"
-require 'rspec/expectations'
-World(::RSpec::Matchers)
+
+begin
+  require 'rspec/expectations'
+  World(::RSpec::Matchers)
+rescue LoadError => e
+  require 'spec/expectations'
+  require 'spec/runner/differs/default'
+  require 'ostruct'
+
+  options = OpenStruct.new(:diff_format => :unified, :context_lines => 3)
+  Spec::Expectations.differ = Spec::Expectations::Differs::Default.new(options)
+  World(Spec::Matchers)
+end
